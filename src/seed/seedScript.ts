a1e4cbfccbbcafe44d8fc78a68909bc96ce99871
Daniel <danielchochlisnki@hotmail.com>
2023-04-10 11:04:29 +0200

    initial seed script

--- src/seed/seedScript.ts
+++ src/seed/seedScript.ts
@@ -1,13 +1,14 @@
 import { connectDB } from "../config/db";
 import { User } from "../models/userModel";
 import { users } from "./mockup/users.mockup";
-connectDB();
 const seedDB = async () => {
+  connectDB();
   try {
-    await User.deleteMany();
+    await User.deleteMany({});
     await User.insertMany(users);
+    console.log("DB seeded successfully");
   } catch (err) {
-    console.log(err);
+    console.error("Error seeding database:", err);
     process.exit(1);
   }
 };
