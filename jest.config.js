f4d57567a06234ab7b5b1400901e8dfa8e9abab2
Daniel <danielchochlisnki@hotmail.com>
2023-04-11 20:47:36 +0200

    initilize testing folder and setup

--- jest.config.js
+++
@@ -1,7 +0,0 @@
-export const transform = {
-  "^.+\\.ts?$": "ts-jest",
-};
-export const testEnvironment = "node";
-export const testRegex = "./src/.*\\.(test|spec)?\\.(js|ts)$";
-export const moduleFileExtensions = ["ts", "js", "json"];
-export const roots = ["<rootDir>/src"];
--- src/server.ts
+++ src/server.ts
@@ -2,7 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 dotenv.config();
 import bodyParser from "body-parser";
-const app = express();
+export const app = express();
 const port = process.env.PORT;
 import authRoute from "./routes/auth";
 import dashboardRoute from "./routes/dashboard";
