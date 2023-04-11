ef7acbbf8e23910bf0f7f47aa6c66cb02fa1c4fb
Daniel <danielchochlisnki@hotmail.com>
2023-04-11 10:36:22 +0200

    0.005

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,12 +1,12 @@
 import mongoose from "mongoose";
-import { config } from "./config";
+// import { config } from "./config";
 
 // const MONGO_DB_URL = process.env.MONGO_DB_URL || "";
 // const MONGO_DB_URL_DEV = process.env.MONGO_DB_URL_DEV || "";
 // const MONGO_DB_HERE = "mongodb://localhost:27017";
-const { MONGO_USER, MONGO_PASSWORD, MONGO_IP, MONGO_PORT } = config;
+// const { MONGO_USER, MONGO_PASSWORD, MONGO_IP, MONGO_PORT } = config;
 // const test = `mongodb://bug-tracker:daniel1@mongo:27017/?authSource=admin`;
-const MONGO_URL = `mongodb://${MONGO_USER}:${MONGO_PASSWORD}@${MONGO_IP}:${MONGO_PORT}/?authSource=admin`;
+// const MONGO_URL = `mongodb://${MONGO_USER}:${MONGO_PASSWORD}@${MONGO_IP}:${MONGO_PORT}/?authSource=admin`;
 
 export const connectDB = async () => {
   try {
